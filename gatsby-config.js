f140c96a2f44b616217d1eadeea736aac4ad1f7d
Omar Houmz <omhoumz@gmail.com>
2020-05-10 16:29:43 +0000

    chore: config things

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pomodoro Goals`,
+    description: `Manage your time with goals`,
+    author: `@omarhoumz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Pomodoro Goals`,
+        short_name: `Pomodoro`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
