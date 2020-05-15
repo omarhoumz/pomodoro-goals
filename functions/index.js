d5ce35e8b8de48b5e3fd0413c383966d1be14663
Omar Houmz <omhoumz@gmail.com>
2020-05-15 00:10:05 +0000

    chore: slaaaash 🤦‍♂️

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ const app = express()
 const corsOptions =
   process.env.NODE_ENV === 'production'
     ? {
-        origin: 'https://pomodoro-goals.netlify.app/',
+        origin: 'https://pomodoro-goals.netlify.app',
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
       }
     : { origin: true }
